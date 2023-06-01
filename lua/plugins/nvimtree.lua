f2ca4d1922d5862ea498cd8d44e43039d307e79c
Larissa Reis <larissa@amadteaparty.org>
2023-06-01 20:08:08 -0300

    Customize nvim-tree mappings

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,10 +1,38 @@
 local M = {}
 
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- apply default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- apply custom mappings
+  vim.keymap.set('n', 'o', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', 'O', api.node.open.horizontal, opts('Open: Horizontal Split'))
+  vim.keymap.del('n', '<BS>', { buffer = bufnr })
+  vim.keymap.set('n', '-', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', 'h', '5zh', opts('Move screen to left'))
+  vim.keymap.set('n', 'l', '5zl', opts('Move screen to right'))
+end
+
+
 function M.setup()
   require('nvim-tree').setup(
     {
+      on_attach = on_attach,
       auto_reload_on_write = false,
       sync_root_with_cwd = true,
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
       renderer = {
         highlight_git = true,
         full_name = false,
