61416c94178c4ff9941fe098f94f43688aa1ad29
Larissa Reis <larissa@amadteaparty.org>
2023-06-05 13:03:56 -0300

    Group plugins by type

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,28 +19,24 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  -- Editing / movements
+  {
+    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "RRethy/nvim-treesitter-textsubjects",
+      "RRethy/nvim-treesitter-endwise",
+    },
+    config = function()
+      require("plugins/treesitter").setup()
+    end
+  },
   {
     "scrooloose/nerdcommenter",
     config = function()
       -- add space around comment delimiters on NERDCommenter
       vim.g.NERDSpaceDelims = 1
       vim.g.NERDDefaultAlign = 'left'
-    end,
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("plugins/nvimtree").setup()
-    end,
-  },
-  {
-    "jremmen/vim-ripgrep",
-    config = function()
-      -- map ripgrep search to <leader>s[search]
-      utils.nmap("<Leader>s", ":Rg<CR>")
-      utils.vmap("<Leader>s", ":call RgVisual()<CR>")
     end,
   },
   {
@@ -51,20 +47,40 @@ require("lazy").setup({
     end,
   },
   {
-    "w0rp/ale",
-    init = function()
-      -- customize linter signs
-      vim.g.ale_sign_error = "●"
-      vim.g.ale_sign_warning = "•"
-      -- customize linter colours
-      vim.cmd("highlight link ALEErrorSign DiffDelete")
-      -- only lint when leaving insert mode after an edit
-      vim.g.ale_lint_on_text_changed = "normal"
-      vim.g.ale_lint_on_insert_leave = 1
-      -- show error details with m[ore details]
-      utils.nmap("<Leader>m", ":ALEDetail<CR>")
+    "echasnovski/mini.surround",
+    version = "*",
+    config = function()
+      require("plugins/surround").setup()
     end
   },
+  {
+    "echasnovski/mini.bufremove",
+    version = '*',
+    config = function()
+      bufremove = require("mini.bufremove")
+      bufremove.setup {}
+      -- key mapping to delete buffer while preserving windows
+      utils.nmap("<Leader>d", bufremove.wipeout)
+    end,
+  },
+  {
+    "unblevable/quick-scope",
+    -- Trigger a highlight in the appropriate direction when pressing these keys
+    init = function()
+      vim.g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
+    end,
+  },
+  {
+    "junegunn/vim-easy-align",
+    config = function()
+      -- start interactive EasyAlign in visual mode (e.g. vip<Enter>)
+      utils.vmap("<Enter>", "<Plug>(EasyAlign)")
+      -- start interactive EasyAlign for a motion/text object (e.g. gaip)
+      utils.nmap("ga", "<Plug>(EasyAlign)")
+    end,
+  },
+  "Lokaltog/vim-easymotion",
+  "jamessan/vim-gnupg",
 
   -- Git integration
   {
@@ -80,16 +96,7 @@ require("lazy").setup({
     end
   },
 
-  {
-    "majutsushi/tagbar",
-    config = function()
-      -- toggle g[o] to navigation
-      utils.nmap("<Leader>g", ":TagbarToggle<CR>")
-      vim.g.tagbar_autoclose = 1
-      vim.g.tagbar_autofocus = 1
-      vim.g.tagbar_map_help = "<F1>"
-    end,
-  },
+  -- Completion
   {
     "hrsh7th/nvim-cmp",
     config = function()
@@ -116,6 +123,57 @@ require("lazy").setup({
     },
   },
   {
+    "j-morano/buffer_manager.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+      require("buffer_manager").setup {}
+      local bmui = require("buffer_manager.ui")
+      -- key mapping to l[ist] buffers to be managed (e.g bulk close)
+      utils.nmap("<Leader>l", bmui.toggle_quick_menu)
+    end,
+  },
+
+  -- Searching
+  {
+    "junegunn/fzf", build = "./install --bin",
+    dependencies = "junegunn/fzf.vim",
+  },
+  {
+    "jremmen/vim-ripgrep",
+    config = function()
+      -- map ripgrep search to <leader>s[search]
+      utils.nmap("<Leader>s", ":Rg<CR>")
+      utils.vmap("<Leader>s", ":call RgVisual()<CR>")
+    end,
+  },
+
+  -- UI
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("plugins/indentlines").setup()
+    end,
+    dependencies = "nvim-treesitter/nvim-treesitter",
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("plugins/nvimtree").setup()
+    end,
+  },
+  {
+    "majutsushi/tagbar",
+    config = function()
+      -- toggle g[o] to navigation
+      utils.nmap("<Leader>g", ":TagbarToggle<CR>")
+      vim.g.tagbar_autoclose = 1
+      vim.g.tagbar_autofocus = 1
+      vim.g.tagbar_map_help = "<F1>"
+    end,
+  },
+  {
     "nvim-lualine/lualine.nvim",
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
@@ -134,77 +192,6 @@ require("lazy").setup({
     config = true,
   },
   {
-    "j-morano/buffer_manager.nvim",
-    dependencies = "nvim-lua/plenary.nvim",
-    config = function()
-      require("buffer_manager").setup {}
-      local bmui = require("buffer_manager.ui")
-      -- key mapping to l[ist] buffers to be managed (e.g bulk close)
-      utils.nmap("<Leader>l", bmui.toggle_quick_menu)
-    end,
-  },
-  {
-    "echasnovski/mini.surround",
-    version = "*",
-    config = function()
-      require("plugins/surround").setup()
-    end
-  },
-  {
-    "echasnovski/mini.bufremove",
-    version = '*',
-    config = function()
-      bufremove = require("mini.bufremove")
-      bufremove.setup {}
-      -- key mapping to delete buffer while preserving windows
-      utils.nmap("<Leader>d", bufremove.wipeout)
-    end,
-  },
-  {
-    "junegunn/vim-easy-align",
-    config = function()
-      -- start interactive EasyAlign in visual mode (e.g. vip<Enter>)
-      utils.vmap("<Enter>", "<Plug>(EasyAlign)")
-      -- start interactive EasyAlign for a motion/text object (e.g. gaip)
-      utils.nmap("ga", "<Plug>(EasyAlign)")
-    end,
-  },
-  "Lokaltog/vim-easymotion",
-  {
-    "unblevable/quick-scope",
-    -- Trigger a highlight in the appropriate direction when pressing these keys
-    init = function()
-      vim.g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
-    end,
-  },
-  "jamessan/vim-gnupg",
-  { "neoclide/coc.nvim", branch = "release" },
-
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("plugins/indentlines").setup()
-    end,
-    dependencies = "nvim-treesitter/nvim-treesitter",
-  },
-
-  {
-    "junegunn/fzf", build = "./install --bin",
-    dependencies = "junegunn/fzf.vim",
-  },
-  "dag/vim-fish",
-  {
-    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "RRethy/nvim-treesitter-textsubjects",
-      "RRethy/nvim-treesitter-endwise",
-    },
-    config = function()
-      require("plugins/treesitter").setup()
-    end
-  },
-  {
     "navarasu/onedark.nvim",
     config = function()
       local onedark = require('onedark')
@@ -215,4 +202,22 @@ require("lazy").setup({
       onedark.load()
     end,
   },
+
+  -- LSP plugins
+  {
+    "w0rp/ale",
+    init = function()
+      -- customize linter signs
+      vim.g.ale_sign_error = "●"
+      vim.g.ale_sign_warning = "•"
+      -- customize linter colours
+      vim.cmd("highlight link ALEErrorSign DiffDelete")
+      -- only lint when leaving insert mode after an edit
+      vim.g.ale_lint_on_text_changed = "normal"
+      vim.g.ale_lint_on_insert_leave = 1
+      -- show error details with m[ore details]
+      utils.nmap("<Leader>m", ":ALEDetail<CR>")
+    end
+  },
+  { "neoclide/coc.nvim", branch = "release" },
 })
