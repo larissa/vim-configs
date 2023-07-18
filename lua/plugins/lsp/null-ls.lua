1cd8cdb8b429a74fe208b1355c899479a9a23c98
Larissa Reis <larissa@amadteaparty.org>
2023-07-17 23:16:28 -0400

    Add eslint diagnostics to null-ls

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -9,16 +9,13 @@ M.setup = function()
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
   local formatting = null_ls.builtins.formatting
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-  -- local diagnostics = null_ls.builtins.diagnostics
+  local diagnostics = null_ls.builtins.diagnostics
 
   null_ls.setup({
     debug = false,
     sources = {
       formatting.prettier,
-      -- formatting.eslint,
-      -- formatting.black.with({ extra_args = { "--fast" } }),
-      -- formatting.stylua,
-      -- diagnostics.flake8,
+      diagnostics.eslint,
     },
   })
 end
