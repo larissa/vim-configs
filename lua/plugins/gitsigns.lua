0ff7ae7743c248217e0e463605c2848fb1516046
Larissa Reis <larissa@amadteaparty.org>
2024-07-07 00:47:53 -0400

    Remove deprecated highlight config from gitsigns
    Setting highlights through a setup option was deprecated. Now
    hightlights should be set with nvim's set_hl api. E.g
    
    ```
    'signs.add.hl' is now deprecated, please define highlight 'GitSignsAdd' e.g:
      vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'GitSignsAdd' })
    ```
    
    However, my config wasn't customizing any highlight, so I removed them.

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,36 +6,11 @@ local icons = require("icons")
 M.setup = function()
   require("gitsigns").setup({
     signs = {
-      add = {
-        hl = "GitSignsAdd",
-        text = icons.ui.BoldLineLeft,
-        numhl = "GitSignsAddNr",
-        linehl = "GitSignsAddLn",
-      },
-      change = {
-        hl = "GitSignsChange",
-        text = icons.ui.BoldLineLeft,
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
-      delete = {
-        hl = "GitSignsDelete",
-        text = icons.ui.Triangle,
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      topdelete = {
-        hl = "GitSignsDelete",
-        text = icons.ui.Triangle,
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      changedelete = {
-        hl = "GitSignsChange",
-        text = icons.ui.BoldLineLeft,
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
+      add = { text = icons.ui.BoldLineLeft },
+      change = { text = icons.ui.BoldLineLeft },
+      delete = { text = icons.ui.Triangle },
+      topdelete = { text = icons.ui.Triangle },
+      changedelete = { text = icons.ui.BoldLineLeft },
     },
     current_line_blame_opts = {
       virt_text = true,
