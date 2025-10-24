4b9f2d03ccdc2db75b5111c9522834b74387ddec
Larissa Reis <lreis@doximity.com>
2025-10-24 15:08:46 -0400

    Update lspconfig api to fix deprecation message

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,8 +1,3 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
 require("plugins.lsp.mason").setup()
 require("plugins.lsp.handlers").setup()
 require("plugins.lsp.null-ls").setup()
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -31,11 +31,6 @@ M.setup = function()
     automatic_installation = true,
   })
 
-  local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-  if not lspconfig_status_ok then
-    return
-  end
-
   local opts = {}
 
   for _, server in pairs(servers) do
@@ -51,7 +46,7 @@ M.setup = function()
       opts = vim.tbl_deep_extend("force", conf_opts, opts)
     end
 
-    lspconfig[server].setup(opts)
+    vim.lsp.config(server, opts)
   end
 end
 
